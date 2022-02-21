b38616b230616e770aeb7485ccaf348191218df1
mikeLofton <michaellofton32@gmail.com>
2022-02-21 12:59:10 -0600

    Woked on seek

--- raygame/SeekBehaviour.h
+++ raygame/SeekBehaviour.h
@@ -1,7 +1,21 @@
 #pragma once
 #include "Component.h"
+#include <Vector2.h>
+class MovementComponent;
+class Actor;
+
 class SeekBehaviour :
 	public Component
 {
+public:
+	SeekBehaviour(const char* name = "SeekBehaviour") : Component::Component(name) {}
+
+	void update(float deltaTime) override;
+
+private:
+	MovementComponent* m_moveComponent;
+	Actor* m_target;
+	MathLibrary::Vector2 m_desiredVelocity;
+	MathLibrary::Vector2 m_steeringForce;
 };
 
